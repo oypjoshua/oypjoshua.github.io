a17dcdedaff8a99875c653646926b741dedef6f2
oypjoshua <75962879+oypjoshua@users.noreply.github.com>
2020-12-14 13:58:32 +0800

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ class NavBar extends Component {
     render() { 
         return ( 
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a className="navbar-brand" href="#">Best dota player!</a> 
+        <a className="navbar-brand" href="https://blog.dota2.com/?l=english">Best dota player!</a> 
         </nav>
         );
     }
